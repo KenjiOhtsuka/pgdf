9911960afbd6fe8db518de86ed20b70c6e73180f
Kenji Ohtsuka <kok.fdcm@gmail.com>
2024-01-12 02:26:23 +0900

    fix regex

--- pgdf/blame.py
+++ pgdf/blame.py
@@ -20,20 +20,32 @@ class LineBlame:
         """
         Parse a line blame from a line.
 
-        >>> blame = LineBlame.parse('0e2b5b3d (Mael Kim 2019-11-04 23:04:00 +0900  1) import re')
+        >>> blame = LineBlame.parse('0e2b5b3c (Kenji Otsuka 2019-11-04 23:04:00 +0900  1) import re')
         >>> blame.commit_hash
-        '0e2b5b3d'
+        '0e2b5b3c'
         >>> blame.author
-        'Mael Kim'
+        'Kenji Otsuka'
         >>> blame.datetime
         '2019-11-04 23:04:00 +0900'
         >>> blame.line_number
         1
 
-        :param line: like '0e2b5b3d (Mael Kim 2019-11-04 23:04:00 +0900  1) import re'
+        >>> blame = LineBlame.parse('8e30324f6 (kenjiotsuka   2022-01-07 15:04:40 +0900 1085) // アクセストークン取得')
+        >>> blame.commit_hash
+        '8e30324f6'
+        >>> blame.author
+        'kenjiotsuka'
+        >>> blame.datetime
+        '2022-01-07 15:04:40 +0900'
+        >>> blame.line_number
+        1085
+
+
+        :param line: like '0e2b5b3c (Kenji Otsuka 2019-11-04 23:04:00 +0900  1) import re'
         :return:
         """
-        m = re.match(r'^\s*(?P<commit_hash>[0-9a-f]{8,40})\s+\((?P<author>.+?)\s+(?P<datetime>(?P<date>[-\d]+?) (?P<time>[:\d]*?) (?P<timezone>.*?)) \s+(?P<line_number>\d+)\)\s+(?P<line>.*)$', line)
+        # print(line)
+        m = re.match(r'^\s*(?P<commit_hash>[0-9a-f]{6,40})\s+\((?P<author>.+)\s+(?P<datetime>(?P<date>[-\d]+?) (?P<time>[:\d]*?) (?P<timezone>.*?))\s+(?P<line_number>\d+)\)\s+(?P<line>.*)$', line)
         if m:
             commit_hash = m.group('commit_hash')
             author = m.group('author')
