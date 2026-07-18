069d1b4bdcef18faf8ed3d7e847c65759ca06a44
Kenji Otsuka <kok.fdcm@gmail.com>
2026-07-19 02:17:19 +0900

    fix workflow

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     packages=find_packages(),
     # package_dir={'': 'pgdf'},
     # packages=find_packages(where='pgdf'),
-    version='0.0.2',
+    version='0.0.3',
     py_modules=['pgdf', 'pgdf.main', 'pgdf.summary'],
     install_requires=[
         'xlsxwriter',
